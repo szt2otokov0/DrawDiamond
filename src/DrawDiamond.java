7d1e0b3e398ed4fae737c5a82aa8821eabd34a13
szt2otokov <szt2otokov@vasvari.org>
2023-09-21 11:11:38 +0200

    all halves work

--- src/DrawDiamond.java
+++ src/DrawDiamond.java
@@ -7,14 +7,27 @@ public class DrawDiamond {
         System.out.println("Adj meg egy számot:");
         Scanner scn = new Scanner(System.in);
         int height = scn.nextInt();
+        int length = 1;
 
-        for(int i = 1; i < height*2-1;i++){
-            for(int j = 0; j < (height-i)/2;j++){
+        for(int i = 0; i < height;i++){
+            for(int j = 0; j < height-i;j++){
                 drawSpace.run();
             }
-            for(int j = 0; j < i;j++){
+            for(int j = 0; j < length;j++){
                 drawStar.run();
             }
+            length += 2;
+            System.out.print('\n');
+        }
+        length -= 4;
+        for(int i = height; length > 0;i--){
+            for(int j = 0; j < height-(i-2);j++){
+                drawSpace.run();
+            }
+            for(int j = 0; j < length;j++){
+                drawStar.run();
+            }
+            length -= 2;
             System.out.print('\n');
         }
     }
